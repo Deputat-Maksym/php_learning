3cba9cd4c14be276bb6a76d9fdfda2a2d7f16cb1
deputat_maksym@i.ua <deputat_maksym@i.ua>
2019-08-16 14:29:04 +0300

    home work - lesson 2

---
+++ lesson_2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 2019;
+
+echo 'a = ' . $a . '<br>';
+
+echo 'type of "a" variable - ';
+var_dump($a);
+echo '<br><br>';
+
+define('NAME', 'Maksym');
+$age = 35;
+
+echo 'My name is ' . NAME . '<br>';
+echo 'I`m  ' . $age . ' old';
+
+unset($age);
